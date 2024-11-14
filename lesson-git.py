df0c381aae1fa4cd2e38172dd5e48347fb368829
D M <danm0rboss@gmail.com>
2024-11-14 13:24:12 +0100

    Impement new feature

--- lesson-git.py
+++ lesson-git.py
@@ -1,3 +1,14 @@
 """
 1st stage  - working directory - untracked
 """
+
+print("Hello! I'm a new feature")
+
+"""
+MAIN - main code - actuallast changes made
+
+1. Create new branch and checkout from there
+2. Add changes, commit them push to the branch
+3. Merge into main
+
+"""
